2793e67e22b3f6eaa5bd34661719d69348fe8864
lxl-xiaolong <113812530+lxl-xiaolong@users.noreply.github.com>
2023-02-26 13:35:48 +0800

    Delete link number20230225.js

--- js/link number20230225.js
+++
@@ -1,5 +0,0 @@
-//link number
-	temp = document.getElementById("content");
-	lis = temp.getElementsByTagName("li");
-	var linum = lis.length; //显示li元素的个数
-	LiNum.innerHTML = "本站各板块已收录网站链接总计：" + linum + "个。"
